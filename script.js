07ec51a9f9d288e29a19d0432c7eb4b5d07080ae
avez07 <ansariaavez12345@gmail.com>
2023-01-11 22:07:19 +0530

    java tutorials srats

---
+++ script.js
@@ -0,0 +1,86 @@
+console.log("hello world");
+
+//variables
+ let Name = "ansari avez";
+ //define the variables and asign the values to it 
+ console.log(Name)
+ alert(Name);
+
+ // define the variables hello and assign the value as avez
+ let hello = "avez";
+ console.log(hello);
+
+ let message ;
+ message = "hello";
+ message ="world";
+
+ console.log(message);
+
+ //Declear two variables : admin and name.
+ //Asign the value "john " to name .
+ //Copy the value from name to admin
+ //Show the value in alert (must output "john")
+  let admin;
+  let name;
+  name = "john";
+  admin = name;
+//   alert(admin);
+
+// constants
+
+const birthday = "29.11.2002";
+//birthday = "25.05.1999"
+console.log(birthday);
+
+//Dat types in js 
+//1. number
+let n= 123;
+let nf=12.3444;
+console.log(n ,nf);
+
+//2. bigint 
+let bigint = 1234567890n;
+console.log(bigint)
+
+// 3. string
+
+let str = "hellow";
+let str2 = 'single quotes are ok to';
+let pharse = `can embed another ${str2}`;
+console.log(pharse);
+alert(`the result is ${1 + 2}`);
+
+//4. boolean 
+let isgreater = 4<1;
+alert(isgreater);
+
+// 5. null
+//it's just a special valur which represent " notting ",  "empty" or "value unkoun".
+let age = null;
+console.log(age);
+
+//6.undefine 
+let data;
+console.log(data);
+
+//7. object for more complex data structure .
+//8.symbol for unique idenfication .
+
+//basic operaters
+//addition
+let apple = "2";
+let orange = "3";
+alert(apple + orange);
+
+//1. arithmatic operaters 
+a1 = 20;
+a2 =  30;
+a3 = a1 +a2;
+console.log(a3);
+
+let n1 = 2;
+n1+=5; //now n = 7 (same aas n = n + 5 )
+n1*= 2; // now n = 14 (because loc 82 declare the n = 7 so n = n*2)
+console.log( n1 );
+
+
