ece5a6d64953bf0baafed9115980b62ab88bf7a9
avez07 <ansariaavez12345@gmail.com>
2023-01-12 20:44:53 +0530

    javascript updated

--- script.js
+++ script.js
@@ -79,8 +79,84 @@ a3 = a1 +a2;
 console.log(a3);
 
 let n1 = 2;
-n1+=5; //now n = 7 (same aas n = n + 5 )
-n1*= 2; // now n = 14 (because loc 82 declare the n = 7 so n = n*2)
+n1 +=5; //now n = 7 (same aas n = n + 5 )
+n1 *= 2; // now n = 14 (because loc 82 declare the n = 7 so n = n*2)
 console.log( n1 );
+
+let x = 1;
+x = -x;
+alert( x ); // unaru negatie was applied
+
+let z= 1, y=3;
+alert(y - z);
+
+//devision 8 / 3
+alert(8 % 3);
+
+//exponentional 2² 
+alert(2 ** 2); //2² = 4
+alert( 2 ** 3 );// 2³ = 8
+alert(2 ** 4);// 2⁴ = 16
+
+//'1' is string that is why the ansawer is 12 or 21
+console.log('1' + 2);
+console.log(2 + '1');
+console.log(2 + 2 + '1');
+
+//3.string operators
+let s = "my" + "string";
+console.log(s);
+
+//.incremental and decement apertors
+//++x pe incremental .... incremental x by 1 then return x
+//x++ post-increment ... return x than increment by 1 
+// --x pre-decrement ...drement x by 1 then return x
+// x-- post-decrement ... return x than decrement by 1
+
+let a;
+a = 10; 
+console.log(++a);//11
+console.log(a);//11
+
+let g;
+g = 12
+console.log(g++)//12
+console.log(g);//13
+
+let b;
+b = 15;
+console.log(--b);//14
+console.log(b);//14
+
+let v = 24;
+console.log(v--);//24
+console.log(v);//23
+
+//5.logical operators
+//&& logical AND : true if both the operands/ boolean value are , else evalutate to false 
+// || logical OR : true if either of the operand/boolean value is true. evalute to false if both are false 
+// ! logical not :  true if the operand is false and vice-versa.
+
+
+// AND &&
+const c = true , d = false;
+e = 2;
+console.log(c && d); //false
+console.log( c && c); // true 
+console.log((e < 1) && (e > 1)); //false
+
+// ||or 
+const q = true , f = false
+console.log(q || f); // true
+console.log(f || f); // false 
+
+//! NOT
+const j = true , k = false;
+console.log(!j); // false
+console.log(!k); //true
+
+
+
+
 
 
